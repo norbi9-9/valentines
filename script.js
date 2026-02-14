f0031d970d3dc0d93fbf62e163d771a1d2e51439
norbi <n.piotrowski@aol.com>
2026-02-14 13:23:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "yayyyy!";
 
     catImg.src = "cat_dance.gif";
 
